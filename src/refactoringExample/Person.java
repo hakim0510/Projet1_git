98539560064c1cbbd984a34fec93ab12c975465f
1795680 <1795680@LI-S064-0T18>
2018-05-23 09:12:40 -0400

    code

--- src/refactoringExample/Person.java
+++ src/refactoringExample/Person.java
@@ -10,5 +10,9 @@ package refactoringExample;
  * @author 1795680
  */
 public class Person {
+    int age;
+    public void study (){
+        System.out.println("I study. ");
+    }
     
 }
--- src/refactoringExample/Student.java
+++ src/refactoringExample/Student.java
@@ -5,10 +5,48 @@
  */
 package refactoringExample;
 
+import java.util.Date;
+
 /**
  *
  * @author 1795680
  */
-public class Student {
+public class Student extends Person{
+    String student_id;
+    String lastName;
+    String firstName;
+    Date dateBirth;
+
+    public Student(String n, String p, Date d) {
+        this.lastName = n;
+        this.firstName = p;
+        this.dateBirth = d;
+        this.student_id= createStudentId(n,p,d);
+    }
+
+    private String createStudentId(String lName, String fName, Date date) {
+        String end = concatDate(date);
+        return lName.substring(0, 3)+fName.charAt(0)+end;
+        
+    }
+
+    private String concatDate(Date date) {
+        return date.getYear()+" - "+date.getMonth();
+        
+    }
+
+    public void setStudent_id(String student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getStudent_id() {
+        return student_id;
+    }
+    public void isSupervisedBy(Professor professor){
+        professor.students.add(this);
+        
+    }
+    
+    
     
 }
